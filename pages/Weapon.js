b6a90754025ce7ae8f5e3f6cd849ece3c11eb95c
Mainman002 <Mainman002@users.noreply.github.com>
2022-05-25 12:45:56 -0500

    Add Stat And Fixes
    *Update resource list
    *Add stat effects
    *Add icon categories

--- pages/Weapon.js
+++ pages/Weapon.js
@@ -1,104 +1,122 @@
 import {Images} from "./resources.js";
-const img = new Images();
-
-const ctx = canvas.getContext("2d");
-const doc_items = [item_name,flag_common,flag_type,skill,slot,destructable,bitmap,model,material,speed,damage,range,copper,silver,tin,gold,spell_name,spell_trigger,spell_damage,race,equiped_particle,desc]
-let item_values = {name:"",flag_common:"", flag_type:"",skill:"",slot:"",destructable:"",bitmap:"",model:"",material:"",speed:"",damage:"",range:"",bronze:"",silver:"",tin:"",gold:"",race:"",spell_name:"",spell_trigger:"",spell_damage:"",equiped_particle:"",desc:""};
-let combined_values = "";
-let has_bitmap = false;
-
-// Used to initialize project images
-function Loader_Init(image_dict) {
-    return image_dict;
-}
-  
-  // Used on objects to display images
-function Loader_Image(src) {
-    const image = new Image;
-    image.src = src;
-    return image;
-}
-
-function DrawImageSimple (_ctx, _image, _pos, _size, _a) {
-    _ctx.save()
-    _ctx.globalAlpha = _a
-
-    _ctx.drawImage(_image,
-        _pos.x, _pos.y, _size.w, _size.h)
-
-    _ctx.restore()
-    _ctx.globalAlpha = 1.0
-}
-
-function value_changed() {
-    setTimeout(function() {
-        // console.log( combined_values );
-        set_values();
-    }, 100);
-}
-
-function set_values() {
-    for (var i = 0; i < doc_items.length; i++) {
-        item_values[doc_items[i].name] = doc_items[i].value
-	}
-
-    // ["Emerald Tenderizer","['WEAPON']","1H Impact",(RPG_SLOT_PRIMARY,RPG_SLOT_SECONDARY),RPG_ITEM_INDESTRUCTIBLE,"EQUIPMENT/MACE/EMERALD/2","weapons/macebladed.dts",10,10,2,5,"","","",("Tenderize",RPG_ITEM_TRIGGER_MELEE,1), ""],
-    // ['Emerald Tenderizer',"['WEAPON','flag_COMMON']",'1H Impact',(RPG_SLOT_PRIMARY, RPG_SLOT_SECONDARY),'RPG_ITEM_INDESTRUCTIBLE','EQUIPMENT/MACE/EMERALD/2',weapons/macebladed.dts,10,10,2,0,0,5,0,(Tenderize,RPG_ITEM_TRIGGER_MELEE,1),'An emerald mace with sharp blades.'],
-    if ( item_values['spell_name'] ) {
-        combined_values = `["${item_values['item_name']}",["${item_values['flag_type']}","${item_values['flag_common']}"],"${item_values['skill']}",(${item_values['slot']}),${item_values['destructable']},"${item_values['bitmap']}","${item_values['model']}","${item_values['material']}",${item_values['speed']},${item_values['damage']},${item_values['range']},${item_values['copper']},${item_values['silver']},${item_values['tin']},${item_values['gold']},"${item_values['desc']}","${item_values['equiped_particle']}","${item_values['race']}",("${item_values['spell_name']}",${item_values['spell_trigger']},${item_values['spell_damage']})],`;
-    } else {
-        combined_values = `["${item_values['item_name']}",["${item_values['flag_type']}","${item_values['flag_common']}"],"${item_values['skill']}",(${item_values['slot']}),${item_values['destructable']},"${item_values['bitmap']}","${item_values['model']}","${item_values['material']}",${item_values['speed']},${item_values['damage']},${item_values['range']},${item_values['copper']},${item_values['silver']},${item_values['tin']},${item_values['gold']},"${item_values['desc']}","${item_values['equiped_particle']}","${item_values['race']}",""],`;
-    }
-    item_result.value = combined_values;
-}
-
-// Original For Loop
-// name[0],itemType[1],skill[2],slots[3],flags[4],bitmap[5],
-// model[6],wpnDamage[7],wpnRate[8],wpnRange[9],worthGold[10],
-// material[11],addRace(ob[12]),desc[13],
-// addSpell( ob[14][0], ob[14][1], ob[14][2] ),equippedParticle[15]
-
-// New For Loop
-// name[0],itemType[1],skill[2],slots[3],flags[4],bitmap[5],model[6],material[7],wpnRate[8],wpDamage[9],wpnRange[10],worthCopper[11],worthSilver[12],worthTin[13],worthGold[14],addRace[15],desc[16],addSpell[17],equippedParticle[18]
-
-
-function return_values() { 
-    set_values();
-    setTimeout(function() {
-        value_changed();
-        navigator.clipboard.writeText(combined_values);
-    }, 100);
-}
-
-class Main {
-    constructor () {
-      this.ctx = canvas.getContext('2d')
-      this.window_size = { w: 40, h: 40 }
-      
-      this.images = img.dict;
-    }
-
-    // update(dt) {
-
-    // }
-
-    draw() {
-        DrawImageSimple(ctx, this.images[ item_values["bitmap"] ], {x:0,y:0}, {w:canvas.width,h:canvas.height}, 1)
-    }
-}
 
 addEventListener('load', (e) => {
+    const img = new Images();
+
+    const ctx = canvas.getContext("2d");
+    const doc_items = [item_name,flag_common,flag_type,skill,slot,destructable,bitmap,model,material,speed,damage,range,armor,copper,silver,tin,gold,spell_name,desc,spell_trigger,spell_damage,race,equiped_particle,add_stats]
+    let item_values = {name:"",flag_common:"", flag_type:"",skill:"",slot:"",destructable:"",bitmap:"",model:"",material:"",speed:"",damage:"",range:"",armor:"",bronze:"",silver:"",tin:"",gold:"",desc:"",race:"",spell_name:"",spell_trigger:"",spell_damage:"",equiped_particle:"",add_stats:""};
+    let combined_values = "";
+    let bitmap_items = []
+
+    function set_img_list(main) {
+        main.active_image = bitmap.selectedIndex;
+        
+        if ( main.active_image_type != bitmap_type.selectedIndex ) {
+            main.active_image_type = bitmap_type.selectedIndex;
+            for ( let item in bitmap_items ) {
+                bitmap_items[item].remove (bitmap_items[item])  
+            }
+
+            for (let key in main.images[main.active_image_type]) {
+                let opt = document.createElement('option');
+                opt.name = 'bitmap_type_option';
+                opt.class = 'bitmap_type_option';
+                opt.id = 'bitmap_type_option';
+                opt.value = main.images[main.active_image_type][key][1];
+                opt.innerHTML = main.images[main.active_image_type][key][0];
+                bitmap.appendChild(opt);
+                bitmap_items.push(opt);
+            }
+        } 
+    }
+
+    function DrawImageSimple (_ctx, _image, _pos, _size, _a) {
+        _ctx.save()
+        _ctx.globalAlpha = _a
+
+        _ctx.drawImage(_image,
+            _pos.x, _pos.y, _size.w, _size.h)
+
+        _ctx.restore()
+        _ctx.globalAlpha = 1.0
+    }
+
+    function value_changed() {
+        setTimeout(function() {
+            set_values();
+        }, 100);
+    }
+
+    function set_values() {
+        main.active_image = bitmap.selectedIndex;
+        main.active_image_type = bitmap_type.selectedIndex;
+
+        for (let i = 0; i < doc_items.length; i++) {
+            item_values[doc_items[i].name] = doc_items[i].value
+        }
+        
+        if ( item_values['spell_name'] ) {
+            combined_values = `["${item_values['item_name']}",["${item_values['flag_type']}","${item_values['flag_common']}"],"${item_values['skill']}",(${item_values['slot']}),${item_values['destructable']},"${item_values['bitmap']}","${item_values['model']}","${item_values['material']}",${item_values['speed']},${item_values['damage']},${item_values['range']},${item_values['armor']},${item_values['copper']},${item_values['silver']},${item_values['tin']},${item_values['gold']},"${item_values['desc']}","${item_values['equiped_particle']}","${item_values['race']}",("${item_values['spell_name']}",${item_values['spell_trigger']},${item_values['spell_damage']}), [${item_values['add_stats']}] ],`;
+        } else {
+            combined_values = `["${item_values['item_name']}",["${item_values['flag_type']}","${item_values['flag_common']}"],"${item_values['skill']}",(${item_values['slot']}),${item_values['destructable']},"${item_values['bitmap']}","${item_values['model']}","${item_values['material']}",${item_values['speed']},${item_values['damage']},${item_values['range']},${item_values['armor']},${item_values['copper']},${item_values['silver']},${item_values['tin']},${item_values['gold']},"${item_values['desc']}","${item_values['equiped_particle']}","${item_values['race']}","", [${item_values['add_stats']}] ],`;
+        }
+        item_result.value = combined_values;
+    }
+
+    function return_values() { 
+        set_values();
+        setTimeout(function() {
+            value_changed();
+            navigator.clipboard.writeText(combined_values);
+        }, 100);
+    }
+
+    class Main {
+        constructor () {
+        this.ctx = canvas.getContext('2d')
+        this.window_size = { w: 40, h: 40 }
+        
+        this.images = img.dict;
+        this.active_image = 0;
+        this.active_image_type = 0;
+        }
+
+        // update(dt) {
+
+        // }
+
+        draw() {
+            if (this.images[bitmap_type.selectedIndex]) {
+                if (this.images[bitmap_type.selectedIndex][this.active_image]) {
+                    DrawImageSimple(ctx, this.images[bitmap_type.selectedIndex][this.active_image][2], {x:0,y:0}, {w:canvas.width,h:canvas.height}, 1)
+                }
+            }
+        }
+    }
+
     const main = new (Main)()
 
-	for (var i = 0; i < doc_items.length; i++) {
+	for (let i = 0; i < doc_items.length; i++) {
         item_values[doc_items[i].name] = doc_items[i].value;
 	    // console.log(doc_items[i].name);
 
         doc_items[i].addEventListener("change", function() {
             item_values[doc_items[i]] = doc_items[i];
             set_values();
+            // console.log(bitmap["options"][bitmap.selectedIndex].value)
         });
 	}
+
+    bitmap_type.addEventListener("click", function() {
+        set_img_list(main);
+        set_values();
+    });
+
+    insertStatBtn.addEventListener("click", function() {
+        add_stats.value = add_stats.value + `(${insert_stat.value},${stat_amount.value}),`;
+        set_values();
+    });
 
     copyBtn.addEventListener("click", function() {
         set_values();
@@ -126,3 +144,13 @@ addEventListener('load', (e) => {
     update(0)
 });
 
+    // Original For Loop
+    // name[0],itemType[1],skill[2],slots[3],flags[4],bitmap[5],
+    // model[6],wpnDamage[7],wpnRate[8],wpnRange[9],worthGold[10],
+    // material[11],addRace(ob[12]),desc[13],
+    // addSpell( ob[14][0], ob[14][1], ob[14][2] ),equippedParticle[15]
+    // Original Result
+    // ["Emerald Tenderizer","['WEAPON']","1H Impact",(RPG_SLOT_PRIMARY,RPG_SLOT_SECONDARY),RPG_ITEM_INDESTRUCTIBLE,"EQUIPMENT/MACE/EMERALD/2","weapons/macebladed.dts",10,10,2,5,"","","",("Tenderize",RPG_ITEM_TRIGGER_MELEE,1), ""],
+
+    // New For Loop
+    // name=ob[0],itemType=ob[1],skill=ob[2],slots=ob[3],flags=ob[4],bitmap=ob[5],model=ob[6],material=ob[7],wpnRate=ob[8],wpnDamage=ob[9],wpnRange=ob[10],worthCopper=ob[11],worthSilver=ob[12],worthTin=ob[13],worthGold=ob[14],desc=ob[15],equippedParticle=ob[16],addRace=ob[17],addSpell=ob[18]
--- pages/resources.js
+++ pages/resources.js
@@ -12,25 +12,38 @@ function Loader_Image(src) {
 
 export class Images {
     constructor() {
-        this.dict = {
-            // Ammo Items
-            'EQUIPMENT/AMMO/1': Loader_Image('../src/items/EQUIPMENT/AMMO/1/0_0_0.png'),
-
-            // Bow Items
-            'EQUIPMENT/BOWS/3': Loader_Image('../src/items/EQUIPMENT/BOWS/3/0_0_0.png'),
-
-            // Swords Items
-            'EQUIPMENT/SWORDS/24': Loader_Image('../src/items/EQUIPMENT/SWORDS/24/0_0_0.png'),
-
-            // Axes Items
-            'EQUIPMENT/AXES/ELVEN/SINGLE/1': Loader_Image('../src/items/EQUIPMENT/AXES/ELVEN/SINGLE/1/0_0_0.png'),
-
-            // Maces
-            'EQUIPMENT/MACE/EMERALD/1': Loader_Image('../src/items/EQUIPMENT/MACE/EMERALD/1/0_0_0.png'),
-            'EQUIPMENT/MACE/EMERALD/2': Loader_Image('../src/items/EQUIPMENT/MACE/EMERALD/2/0_0_0.png'),
-            'EQUIPMENT/MACE/EMERALD/3': Loader_Image('../src/items/EQUIPMENT/MACE/EMERALD/3/0_0_0.png'),
-            'EQUIPMENT/MACE/EMERALD/4': Loader_Image('../src/items/EQUIPMENT/MACE/EMERALD/4/0_0_0.png'),
-            'EQUIPMENT/MACE/EMERALD/5': Loader_Image('../src/items/EQUIPMENT/MACE/EMERALD/5/0_0_0.png'),
-        }
+        this.dict = [ [   // Ammo Items
+                ["Arrow_01", 'EQUIPMENT/AMMO/1', Loader_Image('../src/items/EQUIPMENT/AMMO/1/0_0_0.png')],
+            ],[ // Bow Items
+                ["Bow_03", 'EQUIPMENT/BOWS/3', Loader_Image('../src/items/EQUIPMENT/BOWS/3/0_0_0.png')],
+            ],[ // Swords Items
+                ["Sword_01", 'EQUIPMENT/SWORDS/1', Loader_Image('../src/items/EQUIPMENT/SWORDS/1/0_0_0.png')],
+                ["Sword_24", 'EQUIPMENT/SWORDS/24', Loader_Image('../src/items/EQUIPMENT/SWORDS/24/0_0_0.png')],
+                ["Sword_25", 'EQUIPMENT/SWORDS/25', Loader_Image('../src/items/EQUIPMENT/SWORDS/25/0_0_0.png')],
+                 // Shield Swords
+                ["SHIELDBLADEH", 'EQUIPMENT/SWORDS/SHIELDBLADEH', Loader_Image('../src/items/EQUIPMENT/SWORDS/SHIELDBLADEH/0_0_0.png')],
+                ["SHIELDBLADELH", 'EQUIPMENT/SWORDS/SHIELDBLADELH', Loader_Image('../src/items/EQUIPMENT/SWORDS/SHIELDBLADELH/0_0_0.png')],
+                ["SHIELDBLADELV", 'EQUIPMENT/SWORDS/SHIELDBLADELV', Loader_Image('../src/items/EQUIPMENT/SWORDS/SHIELDBLADELV/0_0_0.png')],
+                ["SHIELDBLADERH", 'EQUIPMENT/SWORDS/SHIELDBLADERH', Loader_Image('../src/items/EQUIPMENT/SWORDS/SHIELDBLADERH/0_0_0.png')],
+                ["SHIELDBLADERV", 'EQUIPMENT/SWORDS/SHIELDBLADERV', Loader_Image('../src/items/EQUIPMENT/SWORDS/SHIELDBLADERV/0_0_0.png')],
+                ["SHIELDBLADEV", 'EQUIPMENT/SWORDS/SHIELDBLADEV', Loader_Image('../src/items/EQUIPMENT/SWORDS/SHIELDBLADEV/0_0_0.png')],
+                ["SHIELDLESSBLADE", 'EQUIPMENT/SWORDS/SHIELDLESSBLADE', Loader_Image('../src/items/EQUIPMENT/SWORDS/SHIELDLESSBLADE/0_0_0.png')],
+                ["SHIELDLESSDOUBLEBLADE", 'EQUIPMENT/SWORDS/SHIELDLESSDOUBLEBLADE', Loader_Image('../src/items/EQUIPMENT/SWORDS/SHIELDLESSDOUBLEBLADE/0_0_0.png')],
+            ],[ // Axes Items
+                ["ELVEN_SINGLE", 'EQUIPMENT/AXES/ELVEN/SINGLE/1', Loader_Image('../src/items/EQUIPMENT/AXES/ELVEN/SINGLE/1/0_0_0.png')],
+            ],[ // Maces                
+                ["EMERALD_01", 'EQUIPMENT/MACE/EMERALD/1', Loader_Image('../src/items/EQUIPMENT/MACE/EMERALD/1/0_0_0.png')],
+                ["EMERALD_02", 'EQUIPMENT/MACE/EMERALD/2', Loader_Image('../src/items/EQUIPMENT/MACE/EMERALD/2/0_0_0.png')],
+                ["EMERALD_03", 'EQUIPMENT/MACE/EMERALD/3', Loader_Image('../src/items/EQUIPMENT/MACE/EMERALD/3/0_0_0.png')],
+                ["EMERALD_04", 'EQUIPMENT/MACE/EMERALD/4', Loader_Image('../src/items/EQUIPMENT/MACE/EMERALD/4/0_0_0.png')],
+                ["EMERALD_05", 'EQUIPMENT/MACE/EMERALD/5', Loader_Image('../src/items/EQUIPMENT/MACE/EMERALD/5/0_0_0.png')],
+            ],[ // Shields                
+                ["SHIELDS_04", 'EQUIPMENT/SHIELDS/4', Loader_Image('../src/items/EQUIPMENT/SHIELDS/4/0_0_0.png')],
+                ["BASICWOODBUCKLER", 'EQUIPMENT/SHIELDS/BASICWOODBUCKLER', Loader_Image('../src/items/EQUIPMENT/SHIELDS/BASICWOODBUCKLER/0_0_0.png')],
+                ["BASICSTEELBUCKLER", 'EQUIPMENT/SHIELDS/BASICSTEELBUCKLER', Loader_Image('../src/items/EQUIPMENT/SHIELDS/BASICWOODBUCKLER/0_0_0.png')],
+                ["BASICWOODNSTEELBUCKLER", 'EQUIPMENT/SHIELDS/BASICWOODNSTEELBUCKLER', Loader_Image('../src/items/EQUIPMENT/SHIELDS/BASICWOODNSTEELBUCKLER/0_0_0.png')],
+                ["EPICWOODNSTEELBUCKLER", 'EQUIPMENT/SHIELDS/EPICWOODNSTEELBUCKLER', Loader_Image('../src/items/EQUIPMENT/SHIELDS/EPICWOODNSTEELBUCKLER/0_0_0.png')],
+                ["STEELSHIELD", 'EQUIPMENT/SHIELDS/STEELSHIELD', Loader_Image('../src/items/EQUIPMENT/SHIELDS/STEELSHIELD/0_0_0.png')],
+        ] ]
     }
 }
